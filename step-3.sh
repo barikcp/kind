ed67e9b25f4e242c208b511692e8cc80d90c85ee
barikcp <113718872+barikcp@users.noreply.github.com>
2022-10-14 22:16:50 +0530

    Update step-3.sh

--- step-3.sh
+++ step-3.sh
@@ -1,1 +1,11 @@
+# Install Kind
 
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64
+
+chmod +x ./kind
+
+mv ./kind /usr/local/bin
+
+kind create cluster --name k8s-kind-cluster-cp
+ 
+kubectl get nodes
