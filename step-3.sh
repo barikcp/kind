889c9bd4b4a31fc7adac4170dd458c14e8ce82c2
barikcp <113718872+barikcp@users.noreply.github.com>
2022-10-18 01:06:23 +0530

    Update step-3.sh

--- step-3.sh
+++ step-3.sh
@@ -1,11 +1,11 @@
 # Install Kind
 
-curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64            # Download the binary
 
-chmod +x ./kind
+chmod +x ./kind                                                                 # Give Executable permission  
 
-mv ./kind /usr/local/bin
+mv ./kind /usr/local/bin                                                        # Move the downloaded folder to /bin directory 
 
-kind create cluster --name k8s-kind-cluster-cp
- 
-kubectl get nodes
+kind create cluster --name k8s-kind-cluster-cp                                  # Create KinD cluster
+
+kubectl get nodes                                                               # Check the status of the node
