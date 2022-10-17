fba0b38f24db2b8020fedb41739b9299aa7db528
barikcp <113718872+barikcp@users.noreply.github.com>
2022-10-18 01:03:12 +0530

    Update step-1.sh

--- step-1.sh
+++ step-1.sh
@@ -9,7 +9,6 @@ sudo apt-get install \
     lsb-release
 
 sudo mkdir -p /etc/apt/keyrings
-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 echo \
@@ -17,9 +16,7 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update\
-
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 ps -eaf|grep docker   # Check the docker process status
-
 sudo service docker status   # check the docker service status
